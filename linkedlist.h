ab167e961682c6c46fe05a56fb92f25a6c1d35d0
KATANADIE <larichev.po@students.sch2.ru>
2021-10-20 10:52:29 +0300

    First commit

---
+++ linkedlist.h
@@ -0,0 +1,268 @@
+#ifndef LINKEDLIST
+#define LINKEDLIST
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <cmath>
+#include <stack>
+#include <deque>
+#include <string>
+#include <ctime>
+using namespace std;
+template <typename T>
+class Node {
+public:
+	Node* leftptr; 
+	Node* rightptr;
+	T value;
+	Node(T value, Node* newLeft);
+
+	Node(T value, Node* newLeft, Node* newRight);
+
+	Node(Node* newLeft, Node* newRight);
+};
+
+template <typename T>
+//TODO - this could be optimized if we look where are we closer to, head or tail.
+class LinkedList {
+private:
+	Node<T>* head = nullptr;
+	Node<T>* tail = nullptr;
+	int sz = 0;
+public:
+	int size() const { return sz; }
+
+	bool isEmpty() const { return sz = 0; }
+
+	void addFront(T value);
+
+	void addBack(T value);
+
+	void deleteFront();
+
+	void deleteBack();
+
+	void addIndex(int index, T val);
+
+	void deleteIndex(int index);
+
+	void deleteVal(T val);
+
+	int findVal(T val);
+
+	void debugPutOut();
+};
+
+template <typename T>
+
+Node<T>::Node(T val, Node* newLeft) {
+	value = val;
+	leftptr = newLeft;
+}
+
+template <typename T>
+
+Node<T>::Node(T val, Node* newLeft, Node* newRight) {
+	value = val;
+	leftptr = newLeft;
+	rightptr = newRight;
+}
+template <typename T>
+Node<T>::Node( Node* newLeft, Node* newRight) {
+	leftptr = newLeft;
+	rightptr = newRight;
+}
+
+
+template <typename T>
+
+void LinkedList<T>::addFront(T value) {
+	if (head != nullptr) {
+		Node<T>* hd = head;
+
+		Node<T>* pnode = new Node<T>(value, nullptr, nullptr);
+		hd->leftptr = pnode;
+
+		pnode->rightptr = hd;
+		head = pnode;
+		sz++;
+	}
+	else {
+		Node<T>* pnode = new Node<T>(value, nullptr, nullptr);
+		head = pnode;
+		tail = pnode;
+		sz++;
+	}
+}
+
+template <typename T>
+
+void LinkedList<T>::addBack(T value) {
+	if (tail != nullptr) {
+		Node<T>* tl = tail;
+
+		Node<T>* pnode = new Node<T>(value, nullptr, nullptr);
+		tl->rightptr = pnode;
+
+		pnode->leftptr = tl;
+		tail = pnode;
+		sz++;
+	}
+	else {
+		Node<T>* pnode = new Node<T>(value, nullptr, nullptr);
+		tail = pnode;
+		head = pnode;
+		sz++;
+	}
+}
+
+template <typename T>
+
+void LinkedList<T>::deleteFront() {
+	if (sz == 1) {
+		delete head;
+		head = nullptr;
+		tail = nullptr;
+		sz--;
+	}
+	else {
+		sz--;
+		Node<T>* pnode = head->rightptr;
+		delete head;
+		pnode->leftptr = nullptr;
+		head = pnode;
+		if (sz == 0) tail = nullptr;
+	}
+}
+
+
+template <typename T>
+
+void LinkedList<T>::deleteBack() {
+	if (sz == 1) {
+		delete tail;
+		tail = nullptr;
+		head = nullptr;
+		sz--;
+	}
+	else {
+		sz--;
+		Node<T>* pnode = tail->leftptr;
+		delete tail;
+		tail = pnode;
+		pnode->leftptr = nullptr;
+		if (sz == 0) head = nullptr;
+	}
+}
+
+template <typename T>
+void LinkedList<T>::addIndex(int index, T value) {
+	if (index >= sz) return;
+
+	int cnt = 0;
+	Node<T>* curr = head;
+	while (cnt != index) {
+		curr = curr->rightptr;
+		cnt++;
+	}
+	if (index == 0) {
+		addFront(value);
+		return;
+	}
+	if (index == sz) {
+		addBack(value);
+		return;
+	}
+
+	Node<T>* pnode = new Node<T>(value, nullptr, nullptr);
+	pnode->leftptr = curr->leftptr;
+	pnode->rightptr = curr;
+	Node<T>* lft = curr->leftptr;
+	lft->rightptr = pnode;
+	curr->leftptr = pnode;
+	sz++;
+	return;
+}
+
+template <typename T>
+
+void LinkedList<T>::deleteIndex(int index) {
+	if (index == 0) {
+		deleteFront();
+		return;
+	}
+
+	if (index == sz - 1) {
+		deleteBack();
+		return;
+	}
+
+	if (index > sz - 1) return;
+
+	int cnt = 0;
+	Node<T>* curr = head;
+	while (cnt != index) {
+		curr = curr->rightptr;
+		cnt++;
+	}
+
+	(curr->leftptr)->rightptr = curr->rightptr;
+	(curr->rightptr)->leftptr = curr->leftptr;
+	sz--;
+	return;
+}
+
+template <typename T>
+void LinkedList<T>::deleteVal(T value) {
+	while (sz > 0 && head->value == value) {
+		head->rightptr->leftptr = nullptr;
+		delete head;
+		sz--;
+	}
+	while (sz > 0 && tail->value == value) {
+		tail->leftptr->rightptr = nullptr;
+		delete tail;
+		sz--;
+	}
+
+	Node<T>* curr = head;
+	while (curr != nullptr) {
+		if (curr->value == value) {
+			(curr->leftptr)->rightptr = curr->rightptr;
+			(curr->rightptr)->leftptr = curr->leftptr;
+			sz--;
+		}
+		curr = curr->rightptr;
+	}
+}
+
+template <typename T> 
+int LinkedList<T>::findVal(T value) {
+	Node<T>* curr = head;
+	if (sz == 0) return -1;
+
+	int cnt = 0;
+	while (curr->value != value) {
+		curr = curr->rightptr;
+		cnt++;
+	}
+	if (cnt == sz) return -1;
+	return cnt;
+}
+
+template <typename T>
+void LinkedList<T>::debugPutOut() {
+	Node<T>* curr = head;
+	while (curr != nullptr) {
+		cout << curr->value << " <-> ";
+		curr = curr->rightptr;
+	}
+	cout << endl;
+}
+
+
+#endif	// ! LINKEDLIST#pragma once
--- main.cpp
+++ main.cpp
@@ -10,119 +10,36 @@
 #include <deque>
 #include <string>
 #include <ctime>
-#include "helper.h"
+#include "linkedlist.h"
 
 #define int long long
 using namespace std;
 const int INF = 1e13 + 8;
 const int mod = 998244353;
-void BAD_gauss(vector<vector<double>>& mtr, vector<double>& consta) {
-	int sz1 = mtr.size();
-	int sz2 = mtr[0].size();
-	double temp;
 
-	for (int i = 0; i < sz1; ++i) {
-		if (i >= sz2) break;
-		for (int j = 0; j < sz1; ++j) {
-			temp = mtr[j][i];
+signed main() {
+	LinkedList<int> ll;
 
-			for (int k = 0; k < sz2; ++k) {
-				if (mtr[i][i] != 0 && j != i) {
-					mtr[j][k] = mtr[j][k] - (mtr[j][i] / mtr[i][i]) * mtr[i][k];
-				}
-			}
-			if (j - i != 0) {
-				consta[j] = consta[j] - mtr[j][i] / mtr[i][i] * consta[i];
-			}
-		}
-	}
+	ll.addFront(3);
+	ll.addBack(4);
 
-	for (int i = 0; i < sz1; ++i) {
-		for (int j = 0; j < sz2; ++j) {
-			if (mtr[i][j] != 0) break;
+	ll.debugPutOut();
+	ll.deleteFront();
+	ll.deleteFront();
 
-			if (j == sz2 - 1 && mtr[i][j] == 0 && consta[i] != 0) {
-				cout << "This system does not have solutions." << endl;
-				return;
-			}
+	ll.debugPutOut();
 
-		}
-	}
+	ll.addBack(9);
+	ll.debugPutOut();
+	ll.addFront(3);
+	ll.debugPutOut();
+	ll.addIndex(1, 2);
+	ll.debugPutOut();
 
-	cout << "The list of free variables is as follows:" << endl;
-
-	for (int it = sz1; it < sz2; ++it) {
-		cout << "X" << it << endl;
-	}
-
-	// this is bad
-
-	for (int iter = sz1 - 1; iter >= 0; iter--) {
-		cout << "x" << iter + 1 << " ";
-	}
-	cout << endl;
-
-	for (int i = sz1 - 1; i >= 0; --i) {
-		cout << "x" << i + 1 << " = " << consta[i] / mtr[i][i] << " - ";
-		for (int k = i + 1; k < sz2; ++k) {
-			double x = mtr[i][k] / mtr[i][i];
-
-			if (k + 1 != sz2) {
-				if (x != 0 && x != 1) {
-					cout << "x" << k + 1 << " - ";
-				}
-				if (x == 1) cout << "x" << k + 1 << " - ";
-			}
-
-			else {
-				if (x != 0 && x != 1) cout << x << "x" << k + 1;
-				if (x == 1) cout << "x" << k + 1;
-			}
-		}
-
-	}
-
-	cout << endl;
-}
-void solve() {
-	int T = 0;
-	cin >> T;
-	while (T--) {
-		int N = 0,
-			M = 0;
-		cin >> N >> M;
-		vector<vector<double>> matr;
-		vector<double> ans;
-		ans.assign(N, 0);
-		matr.assign(N, vector<double>(M , 0));
-		if (N != M) {
-			for (int i = 0; i < N; ++i) {
-				for (int j = 0; j < M; ++j) {
-					cin >> matr[i][j];
-				}
-				cin >> ans[i];
-			}
-
-			for (int i = 0; i < N; ++i) {
-				for (int j = 0; j < M - 1; ++j) {
-					cout << matr[i][j] << " ";
-				}
-				cout << endl;
-			}
-
-
-			BAD_gauss(matr, ans);
-		}
-
-		else {
-			cout << "Please refer to the square function inside of the Matrix function" << endl;
-		}
-	}
-}
-signed main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	solve();
+	ll.deleteIndex(1);
+	ll.debugPutOut();
+	ll.addBack(16);
+	ll.deleteVal(16);
+	ll.debugPutOut();
+	cout << ll.findVal(9) << endl;
 }
